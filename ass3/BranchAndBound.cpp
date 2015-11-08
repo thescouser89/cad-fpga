8853925b522ffbc7c088646f7dfcb0a1c0aac585
Dustin Kut Moy Cheung <thescouser_89@yahoo.co.uk>
2015-11-08 10:08:00 -0500

    Adding initial commit for branch and bound

--- ass3/BranchAndBound.cpp
+++ ass3/BranchAndBound.cpp
@@ -1,5 +1,52 @@
 #include "BranchAndBound.h"
 
-namespace BranchAndBound {
+#include <memory>
+#include <queue>
+#include <vector>
 
+#include "Netlist.h"
+#include "Node.h"
+
+namespace BranchAndBound {
+    void depth_first_search(shared_ptr<vector<int>> order) {
+
+    }
+
+    void breadth_first_search(shared_ptr<vector<int>> order) {
+
+        int best = 50000000000;
+
+        queue<shared_ptr<Node::Node>> q;
+
+        shared_ptr<Node::Node> root = Node::create_root(order);
+        q.push(root);
+
+        while (!q.empty()) {
+            shared_ptr<Node::Node> evaluate = q.front();
+            q.pop();
+
+            shared_ptr<Node::Node> left_child = Node::get_branch(evaluate,
+                                                                 order,
+                                                                 Node::Direction::Left);
+            shared_ptr<Node::Node> right_child = Node::get_branch(evaluate,
+                                                                  order,
+                                                                  Node::Direction::Right);
+            if (left_child->calculate_lower_bound() < best) {
+                // TODO: still have to evaluate if solution is balanced
+                q.push(left_child);
+            }
+
+            if (right_child->calculate_lower_bound() < best) {
+                q.push(right_child);
+            }
+        }
+    }
+
+    void lowest_bound_first(shared_ptr<vector<int>> order) {
+
+    }
+
+    void parallel_breadth_first_search(shared_ptr<vector<int>> order) {
+
+    }
 }
--- ass3/BranchAndBound.h
+++ ass3/BranchAndBound.h
@@ -1,8 +1,20 @@
 #ifndef ASS3_BRANCHANDBOUND_H
 #define ASS3_BRANCHANDBOUND_H
 
-namespace BranchAndBound {
+#include <memory>
+#include <vector>
 
+#include "Netlist.h"
+#include "Node.h"
+
+using namespace std;
+
+namespace BranchAndBound {
+    void depth_first_search(shared_ptr<vector<int>> order);
+    void breadth_first_search(shared_ptr<vector<int>> order);
+    void lowest_bound_first(shared_ptr<vector<int>> order);
+
+    void parallel_breadth_first_search(shared_ptr<vector<int>> order);
 }
 
 #endif //ASS3_BRANCHANDBOUND_H
